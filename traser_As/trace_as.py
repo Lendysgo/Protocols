4737853f663c34cc4e30611597019146d24549a4
Ярослав <110774623+Lendysgo@users.noreply.github.com>
2024-03-31 02:00:14 +0500

    Update trace_as.py
    del

--- traser_As/trace_as.py
+++ traser_As/trace_as.py
@@ -10,20 +10,15 @@ IP_REGEX = r'(?:\d{1,3}\.){3}\d{1,3}'
 def get_ip(ip):
     ip = {}
     try:
-        # Получаем информацию о IP-адресе
         res = IPWhois(ip).lookup_rdap()
     except Exception as e:
         res = {}
-    # Получаем номер автономной системы
     ip['asn'] = res.get('asn')
-    # Получаем страну
     ip['country'] = res.get('asn_country_code')
-    # Получаем провайдера
     ip['provider'] = res.get('network', {}).get('name')
     return ip
 
 def tracert(host):
-    # Запускаем консольную утилиту tracert и возвращаем результат ее работы
     return subprocess.run(f'tracert -d -w 50 {host}',
                           capture_output=True, text=True).stdout
 
@@ -40,11 +35,8 @@ def main():
     parser = get_arg_parser()
     args = parser.parse_args()
     host = args.host
-
-    # Достаем все ip адреса из результата трассировки
     addresses = re.findall(IP_REGEX, tracert(host))[1:]
     table = PrettyTable(['#', 'ip', 'asn', 'country', 'provider'])
-    # Получаем информацию об ip адресах
     res = [(get_ip(ip)) for ip in addresses]
     n = 1
 
